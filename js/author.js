b97aca420a6ecdd3f0f9fc89ed7d441b3664167f
Akashe <160213887+AkasheDev@users.noreply.github.com>
2025-12-28 14:46:13 +0300

    Add author information to the DOM on load

---
+++ js/author.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const authorElement = document.getElementById("akashe-author");
+    
+    if (authorElement) {
+        authorElement.innerHTML = ' | <a href="https://akashe.dev" target="_blank" title="Web Developer" style="text-decoration:none; font-weight:bold; color:inherit;">Designed by Akashe Dev</a>';
+    }
+});
